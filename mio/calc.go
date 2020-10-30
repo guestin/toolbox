c8720e6ed796f6bd21c47fdfe8b0696cb43815dc
suzhen <suzhen@guestin.cn>
2020-10-30 16:32:06 +0800

    :sparkles: add convince functions of md5 family

--- mio/calc.go
+++ mio/calc.go
@@ -220,3 +220,48 @@ func Md5Hex(str string) string {
 	md5Bytes := md5.Sum([]byte(str))
 	return hex.EncodeToString(md5Bytes[:])
 }
+
+func Md5HexFile(fileName string) (string, error) {
+	file, err := os.Open(fileName)
+	if err != nil {
+		return "", err
+	}
+	return Md5HexFile2(file)
+}
+
+func Md5HexFile2(file *os.File) (string, error) {
+	md5Hash := md5.New()
+	if _, err := io.Copy(md5Hash, file); err != nil {
+		return "", err
+	}
+	result := md5Hash.Sum(nil)
+	return hex.EncodeToString(result), nil
+}
+
+func Md5HexStream(reader io.Reader) (string, error) {
+	md5Hash := md5.New()
+	if _, err := io.Copy(md5Hash, reader); err != nil {
+		return "", err
+	}
+	result := md5Hash.Sum(nil)
+	return hex.EncodeToString(result), nil
+}
+
+func Md5HexStream2(readSeeker io.ReadSeeker) (string, error) {
+	restore, err := SaveSeekerPos(readSeeker)
+	if err != nil {
+		return "", err
+	}
+	defer restore()
+	return Md5HexStream(readSeeker)
+}
+
+func SaveSeekerPos(seeker io.Seeker) (restore func(), err error) {
+	savedOffset := int64(0)
+	if savedOffset, err = seeker.Seek(0, io.SeekCurrent); err != nil {
+		return nil, err
+	}
+	return func() {
+		_, _ = seeker.Seek(savedOffset, io.SeekStart)
+	}, nil
+}
